321243500120ec30e4afe7154c105a68bfba804d
minder310 <minder309@gmail.com>
2026-08-28 06:46:40 +0800

    建立hello word頁面

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>我的第一個 Laravel 頁面</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+// /代表原始網址
 Route::get('/', function () {
     return view('welcome');
+});
+// /hello 代表原始網址+/hello
+Route::get('/hello', function () {
+    return view('hello');
 });
